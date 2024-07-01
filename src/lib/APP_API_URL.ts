7690b59400f427d517ff9a425ee0134d779f3f61
shadow <lightshadow416@gmail.com>
2024-07-01 17:50:47 +0100

    updating api url

--- src/lib/APP_API_URL.ts
+++ src/lib/APP_API_URL.ts
@@ -1,3 +1,3 @@
 // export const APP_API_URL: string = "https://task-flow-blond.vercel.app";
 // export const APP_API_URL: string = "http://localhost:3000";
-export const APP_API_URL: string = "https://task-flow-ochre-two.vercel.app";
+export const APP_API_URL: string = "https://task-flow-six.vercel.app";
